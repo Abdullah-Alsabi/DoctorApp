b3ca7d7d3ffadeabbb8071157dfc948749e01087
Mu2Almalki <Mushira.Almalki.ruh.js@tuwaiq.edu.sa>
2022-01-12 10:55:54 +0300

    add

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,6 +1,7 @@
 import "./header.css";
 import React from "react";
 import { Container, Navbar, Nav, Button } from "react-bootstrap";
+import {FaHospitalUser}from 'react-icons/fa'
 // import Logo from "../../doctor_appointment_booking_logo_design_4x.jpg";
 const Header = () => {
   return (
@@ -8,8 +9,8 @@ const Header = () => {
       <Navbar bg="light" variant="light">
         <Container>
           <Nav className="me-auto">
-            <Navbar.Brand href="/">Logo</Navbar.Brand>
-            <Nav.Link href="/doctors">Doctors</Nav.Link>
+            <Navbar.Brand href="/"><FaHospitalUser className="logo"  ></FaHospitalUser> <h5 className="h5">Doctor App</h5></Navbar.Brand>
+            {/* <Nav.Link href="/doctors">Doctors</Nav.Link> */}
           </Nav>
         </Container>
       </Navbar>
