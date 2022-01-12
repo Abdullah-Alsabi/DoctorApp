1508147f354437dd8c008bba30d30b648622787f
TurkiHaqawi <Turki.Haqawii@gmail.com>
2022-01-12 12:46:03 +0300

    auth

--- frontend/src/pages/Appointments.js
+++ frontend/src/pages/Appointments.js
@@ -18,18 +18,26 @@ const Appointments = () => {
       }
     };
     getAppointments();
-  }, [app]);
+  }, []);
+
 
   return (
-    <Card>
-      <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <div className="appointments">
+      {app.map((item) => {
+        return (
+          <Card>
+            <Card.Body>
+              <Card.Title>{item.patientName}</Card.Title>
+              <Card.Text>
+                {item.date}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        )
+      })}
+
+      <div className="addAppointments">+</div>
+    </div>
   );
 };
 
